399e3b808af5a2f4153c75f61e582c69d002e9ed
kebabmaneater <laurimikael8@gmail.com>
2025-06-12 04:29:06 +0300

    added hammer

--- init.lua
+++ init.lua
@@ -1,11 +1,10 @@
-local minetestElementsPT = dofile(minetest.get_modpath("plextech").."/items.lua")
+local minetestElementsPT = dofile(minetest.get_modpath("plextech") .. "/items.lua")
 print(minetestElementsPT)
 
 print("Hello from PlexTech")
 
-for i=1, #minetestElementsPT do
-    registerDust(minetestElementsPT[i], 1)
-    registerPlate(minetestElementsPT[i], 1)
-    print("PT: " .. minetestElementsPT[i] .. " registered")
+for i = 1, #minetestElementsPT do
+	registerDust(minetestElementsPT[i], 1)
+	registerPlate(minetestElementsPT[i], 1)
+	print("PT: " .. minetestElementsPT[i] .. " registered")
 end
-
