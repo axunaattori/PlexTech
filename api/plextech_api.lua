50559c6058a47aa0c50c03a620c26d37fe15fbfa
kebabmaneater <laurimikael8@gmail.com>
2025-06-19 05:10:23 +0300

    dev, added active version

--- api/plextech_api.lua
+++ api/plextech_api.lua
@@ -2,4 +2,13 @@ local materials = dofile(minetest.get_modpath("plextech") .. "/api/materials.lua
 
 PlexTechApi = {
 	materials = materials,
+	compat = {
+		set_info = function(pos, text)
+			if WhatIsThisApi == nil then
+				return
+			end
+
+			WhatIsThisApi.set_info(pos, text)
+		end,
+	},
 }
