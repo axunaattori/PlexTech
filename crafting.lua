23403e0866a10f268322f30dd160f9905c8c5947
kebabmaneater <laurimikael8@gmail.com>
2025-06-19 04:33:11 +0300

    collapsed multiple tables into one

--- crafting.lua
+++ crafting.lua
@@ -52,67 +52,58 @@ core.register_craft({
 	},
 })
 
-local minetestElementsPT = {
-	"Bronze",
-	"Copper",
-	"Steel",
-	"Tin",
-	"Gold",
+local minetest_elements = {
+	{ "Bronze", "#e56209" },
+	{ "Copper", "#ec923a" },
+	{ "Steel", "#eeece8" },
+	{ "Tin", "#b4b3b1" },
+	{ "Gold", "#ffd100" },
+	{ "Diamond", "#00C808", 0 },
 }
 
-local minetestElementColors = {
-	bronze = "#e56209",
-	copper = "#ec923a",
-	steel = "#eeece8",
-	tin = "#b4b3b1",
-	gold = "#ffd100",
-}
-
-local PTMaterial = {
-	"Pig iron",
-	"Iron",
-	"Wrought iron",
-}
-
-local PTMaterialColors = {
-	pig_iron = "#b49696",
-	iron = "#c8c8c8",
-	wrought_iron = "#8c6e6e",
+local plextech_materials = {
+	{ "Pig iron", "#b49696" },
+	{ "Iron", "#c8c8c8" },
+	{ "Wrought iron", "#8c6e6e" },
 }
 
 local registerDust = PlexTechApi.materials.registerDust
 local registerPlate = PlexTechApi.materials.registerPlate
 local registerIngot = PlexTechApi.materials.registerIngot
 
-for _, element in pairs(minetestElementsPT) do
-	registerDust(element, 1, minetestElementColors[element:lower()])
-	registerPlate(element, 1, minetestElementColors[element:lower()])
+local function register_elements(elements, with_ingot)
+	for _, element in ipairs(elements) do
+		local name, color, recipe = element[1], element[2], element[3] or 1
+		registerDust(name, recipe, color)
+		registerPlate(name, recipe, color)
+		if with_ingot then
+			registerIngot(name, recipe, color)
+		end
+	end
 end
 
-registerPlate("Diamond", 0, "#00C8C8")
-registerDust("Diamond", 0, "#00C8C8")
-
-for _, element in pairs(PTMaterial) do
-	registerDust(element, 1, PTMaterialColors[element:lower()])
-	registerPlate(element, 1, PTMaterialColors[element:lower()])
-	registerIngot(element, 1, PTMaterialColors[element:lower()])
-end
+register_elements(minetest_elements, false) --ingots already exist
+register_elements(plextech_materials, true)
 
 local normalize = PlexTechApi.materials.normalize
-core.register_on_mods_loaded(function()
-	for _, element in pairs(minetestElementsPT) do
-		local normalized = normalize(element .. "_ingot")
-		local default = "default:" .. normalized
-		if core.registered_craftitems[default] then
-			local def = core.registered_craftitems[default]
-			local groups = table.copy(def.groups)
-			groups[normalized] = 1
-			core.override_item(default, { groups = groups })
+
+minetest.register_on_mods_loaded(function()
+	for _, element in pairs(minetest_elements) do
+		element = element[1]
+		if element[3] ~= 0 then
+			local normalized = normalize(element .. "_ingot")
+			local default = "default:" .. normalized
+			if core.registered_craftitems[default] then
+				local def = core.registered_craftitems[default]
+				local groups = table.copy(def.groups)
+				groups[normalized] = 1
+				core.override_item(default, { groups = groups })
+			end
 		end
 	end
 
-	core.clear_craft({ output = "default:bronze_ingot" })
-	core.register_craft({
+	minetest.clear_craft({ output = "default:bronze_ingot" })
+	minetest.register_craft({
 		type = "cooking",
 		output = "default:bronze_ingot",
 		recipe = "group:bronze_dust",
